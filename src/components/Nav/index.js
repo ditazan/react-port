222fa16904b39c20a740d00c6fe90372c9702b64
dit <ditazanelli@gmail.com>
2022-06-14 20:02:50 -0500

    nav base and assets

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,8 +1,30 @@
 import React from "react";
 
 // PULL IN THE NESSESSARY PROPS
-function Nav(){
-  <header className=""></header>
+function Nav() {
+  <nav className="menu">
+    <div className="">
+      <h1>
+        <a href="#"></a>
+      </h1>
+    </div>
+    <div className="">
+      <ol>
+        <li className="">
+          <a href="#">about</a>
+        </li>
+        <li>
+          <a href="#">web</a>
+        </li>
+        <li>
+          <a href="#">creative tech</a>
+        </li>
+        <li>
+          <a href="#">contact</a>
+        </li>
+      </ol>
+    </div>
+  </nav>;
 }
 
 export default Nav;
