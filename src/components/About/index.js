40c0c9c7b7836446990aa8765e7e54a0384d06c1
dit <ditazanelli@gmail.com>
2022-06-14 19:44:40 -0500

    favicon

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React from "react";
 import coverImage from "../../assets/images/bio-photo.png";
 
 function About() {
   return (
-<section className="my-5 about">
-  <h5 id="about"><mark>Dita is a Creative Technologist and Full Stack Web Developer with a BFA in Design and Technology from Parsons School of design. They are most inspired by their childhood experience of roaming the virtual worlds of mmo's.</mark></h5>
-  <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
-   
-</section>
+    <section className="about">
+      <div className="about-item about-item-top">
+        {" "}
+        <h5 id="about">
+          <mark>
+            Dita is a Creative Technologist and Full Stack Web Developer with a
+            BFA in Design and Technology from Parsons School of design.{" "}
+          </mark>
+        </h5>
+      </div>
+      <div className="about-item about-item-bottom">
+        <img
+          src={coverImage}
+          className=""
+          style={{ width: "100%" }}
+          alt="cover"
+        />
+      </div>
+    </section>
   );
 }
 
