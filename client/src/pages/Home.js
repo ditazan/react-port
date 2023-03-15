9eb07237417aa916e616f88329ba214b6b8589dc
dit <ditazanelli@gmail.com>
2023-03-15 01:09:56 -0500

    merge

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,15 +1,8 @@
 import React from "react";
 
-import Nav from "../components/Nav";
-import Hero from "../components/Hero";
-import About from "../components/About";
-import Contact from "../components/Contact";
-import Crosshair from "../components/Crosshair";
-import ScreenOverlay from "../components/ScreenOverlay";
-import miku from "../assets/images/miku.png";
-import miku2 from "../assets/images/mikuuu.png";
-import finger from "../assets/images/middlefinger.png";
 
+import Hero from "../components/Hero";
+import ScreenOverlay from "../components/ScreenOverlay";
 function Home() {
   return (
     <div>
